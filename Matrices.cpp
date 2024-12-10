6d5ce45546b5413fb9822fc423293b1a736a1dcd
oswaldogonz <133737580+oswaldogonz@users.noreply.github.com>
2024-12-10 12:45:11 -0800

    Update Matrices.cpp

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,126 +1,127 @@
 #include "Matrices.h"
 
-
-Matrix::Matrix(int _rows, int _cols)
+namespace Matrices
 {
-    a=vector<vector<double>>(_rows,vector<double>(_cols,0));
-    rows=_rows;
-    cols=_cols;
-}
-
-Matrix operator+(const Matrix& a, const Matrix& b)
-{
-    if(a.getRows()!=b.getRows()||a.getCols()!=b.getCols())
+    Matrix::Matrix(int _rows, int _cols)
     {
-        throw runtime_error("Error: dimensions must agree");
+        a=vector<vector<double>>(_rows,vector<double>(_cols,0));
+        rows=_rows;
+        cols=_cols;
     }
-    Matrix c(a.getRows(),a.getCols());
-    for(int i=0;i<a.getRows();i++)
+
+    Matrix operator+(const Matrix& a, const Matrix& b)
     {
-        for(int j=0;j<a.getCols();j++)
+         if(a.getRows()!=b.getRows()||a.getCols()!=b.getCols())
         {
-            c(i,j)=a(i,j)+b(i,j);
+            throw runtime_error("Error: dimensions must agree");
         }
-    }
-    return c;
-}
-
-Matrix operator*(const Matrix& a, const Matrix& b)
-{
-    if(a.getCols()!=b.getRows())
-    {
-        throw runtime_error("Error: dimensions must agree");
-    }
-    Matrix c(a.getRows(),b.getCols());
-    for(int i=0;i<a.getRows();i++)
-    {
-        for(int k=0;k<b.getCols();k++)
+        Matrix c(a.getRows(),a.getCols());
+        for(int i=0;i<a.getRows();i++)
         {
-            c(i,k)=0;
             for(int j=0;j<a.getCols();j++)
             {
-                c(i,k)+=a(i,j)*b(j,k);
+                c(i,j)=a(i,j)+b(i,j);
             }
         }
+        return c;
     }
-    return c;
-}
 
-bool operator==(const Matrix& a, const Matrix& b)
-{
-    if(a.getRows()!=b.getRows()||a.getCols()!=b.getCols())
+    Matrix operator*(const Matrix& a, const Matrix& b)
     {
-        return false;
-    }
-    for(int i=0;i<a.getRows();i++)
-    {
-        for(int j=0;j<a.getCols();j++)
+        if(a.getCols()!=b.getRows())
         {
-            if(abs(a(i,j)-b(i,j))>=0.001)
+            throw runtime_error("Error: dimensions must agree");
+        }
+        Matrix c(a.getRows(),b.getCols());
+        for(int i=0;i<a.getRows();i++)
+        {
+            for(int k=0;k<b.getCols();k++)
             {
-                return false;
+                c(i,k)=0;
+                for(int j=0;j<a.getCols();j++)
+                {
+                    c(i,k)+=a(i,j)*b(j,k);
+                }
             }
         }
+        return c;
     }
-    return true;
-}
 
-bool operator!=(const Matrix& a, const Matrix& b)
-{
-    if(a.getRows()!=b.getRows()||a.getCols()!=b.getCols())
+    bool operator==(const Matrix& a, const Matrix& b)
     {
+        if(a.getRows()!=b.getRows()||a.getCols()!=b.getCols())
+        {
+            return false;
+        }
+        for(int i=0;i<a.getRows();i++)
+        {
+            for(int j=0;j<a.getCols();j++)
+            {
+                if(abs(a(i,j)-b(i,j))>=0.001)
+                {
+                    return false;
+                }
+            }
+        }
         return true;
     }
-    for(int i=0;i<a.getRows();i++)
+
+    bool operator!=(const Matrix& a, const Matrix& b)
     {
-        for(int j=0;j<a.getCols();j++)
+        if(a.getRows()!=b.getRows()||a.getCols()!=b.getCols())
         {
-            if(abs(a(i,j)-b(i,j))>=0.001)
+            return true;
+        }
+        for(int i=0;i<a.getRows();i++)
+        {
+            for(int j=0;j<a.getCols();j++)
             {
-                return true;
+                if(abs(a(i,j)-b(i,j))>=0.001)
+                {
+                    return true;
+                }
             }
         }
+        return false;
     }
-    return false;
-}
 
-ostream& operator<<(ostream& os, const Matrix& a)
-{
-    for(int i=0;i<a.getRows();i++)
+    ostream& operator<<(ostream& os, const Matrix& a)
     {
-        for(int j=0;j<a.getCols();j++)
+        for(int i=0;i<a.getRows();i++)
         {
-            os<<setw(10)<<a(i,j);
+            for(int j=0;j<a.getCols();j++)
+            {
+                os<<setw(10)<<a(i,j);
+            }
+            os<<"\n";
         }
-        os<<"\n";
+        return os;
     }
-    return os;
-}
 
 
-RotationMatrix::RotationMatrix(double theta):Matrix(2,2)
-{
-    a[0][0]=cos(theta);
-    a[0][1]=-sin(theta);
-    a[1][0]=sin(theta);
-    a[1][1]=cos(theta);
-}
-
-ScalingMatrix::ScalingMatrix(double scale):Matrix(2,2)
-{
-    a[0][0]=scale;
-    a[0][1]=0;
-    a[1][0]=0;
-    a[1][1]=scale;
-}
-
-TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols):Matrix(2,nCols)
-{
-    for(int i=0;i<nCols;i++)
+    RotationMatrix::RotationMatrix(double theta):Matrix(2,2)
     {
-      a[0][i]=xShift;
-      a[1][i]=yShift;
+        a[0][0]=cos(theta);
+        a[0][1]=-sin(theta);
+        a[1][0]=sin(theta);
+        a[1][1]=cos(theta);
     }
-}
+
+    ScalingMatrix::ScalingMatrix(double scale):Matrix(2,2)
+    {
+        a[0][0]=scale;
+        a[0][1]=0;
+        a[1][0]=0;
+        a[1][1]=scale;
+    }
+
+    TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols):Matrix(2,nCols)
+    {
+        for(int i=0;i<nCols;i++)
+        {
+          a[0][i]=xShift;
+          a[1][i]=yShift;
+        }
+    }
 
 
